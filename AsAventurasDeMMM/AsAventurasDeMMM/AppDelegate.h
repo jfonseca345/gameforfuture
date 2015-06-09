574799f62b151a7bc495b793dd876da9c66af60e
Jucelio Fonseca <juceliofonseca@MacBook-Pro-de-Jucelio.local>
2015-06-09 11:29:42 -0300

    Initial commit

---
+++ AsAventurasDeMMM/AsAventurasDeMMM/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AsAventurasDeMMM
+//
+//  Created by Jucelio Fonseca on 09/06/15.
+//  Copyright (c) 2015 Archbold S.A. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AsAventurasDeMMM/AsAventurasDeMMM/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  AsAventurasDeMMM
+//
+
+//  Copyright (c) 2015 Archbold S.A. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameViewController : UIViewController
+
+@end

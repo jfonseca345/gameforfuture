6d515d2ea7765546cddcc58122725e1d7ce51e75
Henrique de Abreu Amitay <Amitay@MacBook-Pro-de-Henrique.local>
2015-06-10 11:22:53 -0300

    Status Effects enum

---
+++ AsAventurasDeMMM/AsAventurasDeMMM/StatsEffects.swift
@@ -0,0 +1,17 @@
+//
+//  StatsEffects.swift
+//  AsAventurasDeMMM
+//
+//  Created by Henrique de Abreu Amitay on 10/06/15.
+//  Copyright (c) 2015 Archbold S.A. All rights reserved.
+//
+
+import Foundation
+
+enum StatsEffects
+{
+    //Used to define effects like Poison, Buffs, Debuffs
+    case POISON, ATKUP, ATKDOWN, DEFUP, DEFDOWN, PETRIFIED
+    
+    //More to come...?
+}

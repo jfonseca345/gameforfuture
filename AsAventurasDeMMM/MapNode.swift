b8556b1718e60232289aec5306897374408cf522
Jucelio Fonseca <juceliofonseca@MacBook-Pro-de-Jucelio.local>
2015-06-10 13:59:23 -0300

    blank space

---
+++ AsAventurasDeMMM/MapNode.swift
@@ -0,0 +1,13 @@
+//
+//  MapNode.swift
+//  AsAventurasDeMMM
+//
+//  Created by Jucelio Fonseca on 10/06/15.
+//  Copyright (c) 2015 Archbold S.A. All rights reserved.
+//
+
+import SpriteKit
+
+class MapNode: SKSpriteNode {
+   
+}
